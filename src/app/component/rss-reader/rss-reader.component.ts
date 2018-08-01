054e9e7a5ede0f3536eaf78ac71c4710a49539dc
richardkoszegi <richard.koszegi95@gmail.com>
2018-08-01 11:29:47 +0200

    feladat 5

--- src/app/component/rss-reader/rss-reader.component.ts
+++ src/app/component/rss-reader/rss-reader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RssService } from '../../service/rss.service';
 import { RssModel } from '../../model/rss.model';
 
@@ -7,16 +7,17 @@ import { RssModel } from '../../model/rss.model';
   templateUrl: './rss-reader.component.html',
   styleUrls: ['./rss-reader.component.css']
 })
-export class RssReaderComponent implements OnInit {
+export class RssReaderComponent {
 
   rssFeeds: RssModel[];
+  loadButtonLabel= 'Betöltés';
 
   constructor(private rssService: RssService) { }
 
-  ngOnInit() {
+  reloadFeeds() {
     this.rssService.getRssFeed().subscribe((data) => {
       this.rssFeeds = data.items;
-      console.log(this.rssFeeds)
+      this.loadButtonLabel = 'Feedek frissítése';
     })
   }
 
