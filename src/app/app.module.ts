599ead7e515db7d3f915173798063dd33d5d64a0
richardkoszegi <richard.koszegi95@gmail.com>
2018-08-01 11:03:31 +0200

    Feladat 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MatToolbarModule, MatMenuModule, MatButtonModule, MatInputModule, MatCardModule,} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RssReaderComponent } from './component/rss-reader/rss-reader.component';
+import { HomeComponent } from './component/home/home.component';
+
+export const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'rss-reader', component: RssReaderComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RssReaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/rss-reader/rss-reader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rss-reader',
+  templateUrl: './rss-reader.component.html',
+  styleUrls: ['./rss-reader.component.css']
+})
+export class RssReaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
